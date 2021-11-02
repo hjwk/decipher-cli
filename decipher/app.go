48ce33d5dfef6a39f539f86042be122f546482be
Hubert Woszczyk <woszczykh@gmail.com>
2021-11-02 14:26:10 +0100

    Introduce subcommands

--- decipher/app.go
+++ decipher/app.go
@@ -6,42 +6,112 @@ import (
 	"os"
 )
 
+type Command interface {
+	FromArgs([]string) error
+	Run() error
+	Name() string
+}
+
 // CLI runs the decipher command line app and returns its exit status.
 func CLI(args []string) int {
-	var app appEnv
-	err := app.fromArgs(args)
-	if err != nil {
-		return 2
-	}
-	if err = app.run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Runtime error: %v\n", err)
+	if len(args) < 1 {
+		fmt.Fprintf(os.Stderr, "You need to use a subcommand\n")
 		return 1
 	}
+
+	commands := []Command{
+		NewCipherCommand(),
+		NewDecipherCommand(),
+	}
+
+	command := os.Args[1]
+
+	for _, cmd := range commands {
+		if cmd.Name() == command {
+			cmd.FromArgs(os.Args[2:])
+			if err := cmd.Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "Runtime error: %v\n", err)
+				return 1
+			}
+
+			return 0
+		}
+	}
+
 	return 0
 }
 
-type appEnv struct {
-	cipher    string
-	plaintext string
-	shift     int
+type DecipherCommand struct {
+	flagset    *flag.FlagSet
+	cipher     string
+	ciphertext string
+	lang       string
 }
 
-func (app *appEnv) fromArgs(args []string) error {
-	fl := flag.NewFlagSet("cipher", flag.ContinueOnError)
-	fl.StringVar(&app.cipher, "c", "caesar", "Cipher")
-	fl.StringVar(&app.plaintext, "t", "", "Plaintext")
-	fl.IntVar(&app.shift, "s", 3, "Shift")
+func NewDecipherCommand() *DecipherCommand {
+	cmd := DecipherCommand{flagset: flag.NewFlagSet("decipher", flag.ContinueOnError)}
 
-	if err := fl.Parse(args); err != nil {
+	cmd.flagset.StringVar(&cmd.cipher, "c", "caesar", "Cipher algorithm")
+	cmd.flagset.StringVar(&cmd.ciphertext, "t", "", "Ciphertext")
+	cmd.flagset.StringVar(&cmd.lang, "l", "eng", "Supposed language of the ciphertext")
+
+	return &cmd
+}
+
+func (cmd *DecipherCommand) FromArgs(args []string) error {
+	if err := cmd.flagset.Parse(args); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (app *appEnv) run() error {
-	answer := caesar(app.plaintext, app.shift)
+func (cmd *DecipherCommand) Run() error {
+	answers := breakCaesar(cmd.ciphertext, cmd.lang)
+
+	for _, solution := range answers {
+		fmt.Println(solution)
+	}
+
+	return nil
+}
+
+func (cmd *DecipherCommand) Name() string {
+	return cmd.flagset.Name()
+}
+
+type CipherCommand struct {
+	flagset   *flag.FlagSet
+	cipher    string
+	plaintext string
+	shift     int
+}
+
+func NewCipherCommand() *CipherCommand {
+	cmd := CipherCommand{flagset: flag.NewFlagSet("cipher", flag.ContinueOnError)}
+
+	cmd.flagset.StringVar(&cmd.cipher, "c", "caesar", "Cipher")
+	cmd.flagset.StringVar(&cmd.plaintext, "t", "", "Plaintext")
+	cmd.flagset.IntVar(&cmd.shift, "s", 3, "Shift")
+
+	return &cmd
+}
+
+func (cmd *CipherCommand) FromArgs(args []string) error {
+	if err := cmd.flagset.Parse(args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cmd *CipherCommand) Run() error {
+	answer := caesar(cmd.plaintext, cmd.shift)
 
 	_, err := fmt.Print(answer)
 	return err
+}
+
+func (cmd *CipherCommand) Name() string {
+	return cmd.flagset.Name()
 }
--- decipher/caesar.go
+++ decipher/caesar.go
@@ -1,5 +1,12 @@
 package decipher
 
+import (
+	"sort"
+	"strings"
+)
+
+var freqs_eng = "etaoinshrd"
+
 func caesar(in string, shift int) string {
 	shift = (shift%26 + 26) % 26 // [0, 25]
 	bytes := make([]byte, len(in))
@@ -19,4 +26,44 @@ func caesar(in string, shift int) string {
 	}
 
 	return string(bytes)
+}
+
+func breakCaesar(in, lang string) []string {
+	letters := countFrequencies(in)
+
+	var freqs_ref string
+	switch lang {
+	case "eng":
+		freqs_ref = freqs_eng
+	default:
+		freqs_ref = freqs_eng
+	}
+
+	possibilities := make([]string, 5)
+	for i := 0; i < 5; i++ {
+		shift := int(freqs_ref[i]) - int(letters[i])
+		possibilities[i] = caesar(in, shift)
+	}
+
+	return possibilities
+}
+
+func countFrequencies(in string) string {
+	counts := make(map[string]int)
+	for _, r := range in {
+		if r != ' ' && r != ',' {
+			counts[string(r)]++
+		}
+	}
+
+	letters := make([]string, 0, len(counts))
+	for l := range counts {
+		letters = append(letters, l)
+	}
+
+	sort.Slice(letters, func(i, j int) bool {
+		return counts[letters[i]] > counts[letters[j]]
+	})
+
+	return strings.Join(letters, "")
 }
